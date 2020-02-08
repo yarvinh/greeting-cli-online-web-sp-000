98655a742b0cc2cc01c16836c9196a510abf3330
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-08 05:16:06 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Hi! I'm HAL, what's your name?"
-    
-    puts "Hello #{name}. It's nice to meet you."
+   puts "Hello #{name}. It's nice to meet you."
 end
 
 greeting("yarvin")
